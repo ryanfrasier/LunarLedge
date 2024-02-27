e78ef59f7e4e1f1ffc635de005349ec93af8252a
Ryan Frasier <rfrasier977@gmail.com>
2024-02-27 13:30:07 -0500

    health ui

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    private float currentHealth;
+
+    void Start()
+    {
+        // Initialize current health in the Start or Awake method
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        // Subtract damage from current health and clamp the value to ensure its between 0 and startingHealth
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth <= 0)
+        {
+           // Debug.Log("Health is depleted. Implement death or damage reaction.");
+        }
+        else
+        {
+           // Debug.Log("Took damage, but still alive. Current health: " + currentHealth);
+        }
+    }
+}
