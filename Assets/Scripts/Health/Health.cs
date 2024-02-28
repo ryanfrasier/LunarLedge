1712c5e8a3b57de7e07be1cda5f54244c1c49825
Ryan Frasier <rfrasier977@gmail.com>
2024-02-27 19:18:59 -0500

    health ui updates

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
-    private float currentHealth;
+    public float currentHealth { get; private set; }
 
-    void Start()
+    private void Awake()
     {
         // Initialize current health in the Start or Awake method
         currentHealth = startingHealth;
@@ -18,11 +18,17 @@ public class Health : MonoBehaviour
 
         if (currentHealth <= 0)
         {
-           // Debug.Log("Health is depleted. Implement death or damage reaction.");
+            // Debug.Log("Health is depleted. Implement death or damage reaction.");
         }
         else
         {
-           // Debug.Log("Took damage, but still alive. Current health: " + currentHealth);
+            // Debug.Log("Took damage, but still alive. Current health: " + currentHealth);
         }
+    } // This closing brace was missing
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            TakeDamage(1);
     }
 }
--- Assets/Scripts/Health/Healthbar.cs
+++ Assets/Scripts/Health/Healthbar.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Healthbar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image totalhealthBar;
+    [SerializeField] private Image currenthealthBar;
+
+    private void Start()
     {
-        
+        totalhealthBar.fillAmount = playerHealth.currentHealth / 10;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        currenthealthBar.fillAmount = playerHealth.currentHealth / 10;
     }
 }
