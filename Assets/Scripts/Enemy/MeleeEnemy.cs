fa7c469e0d9a317862bb15c6e4de57c69a8ba952
Ryan Frasier <rfrasier977@gmail.com>
2024-04-02 19:39:49 -0400

    added audio

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
-    [Header ("Attack Parameters")]
+    [Header("Attack Parameters")]
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
     [SerializeField] private int damage;
@@ -46,7 +46,7 @@ public class MeleeEnemy : MonoBehaviour
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = 
+        RaycastHit2D hit =
             Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0, Vector2.left, 0, playerLayer);
