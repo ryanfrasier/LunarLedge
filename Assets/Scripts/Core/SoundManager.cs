9de36da0d9a4fe5dbf735e1c9325117804e5fe6d
Ryan Frasier <rfrasier977@gmail.com>
2024-04-18 18:37:41 -0400

    added game over screen and pause menu

--- Assets/Scripts/Core/SoundManager.cs
+++ Assets/Scripts/Core/SoundManager.cs
@@ -20,9 +20,42 @@ public class SoundManager : MonoBehaviour
         //Destroy duplicate gameobjects
         else if (instance != null && instance != this)
             Destroy(gameObject);
+
+        //Assign initial volumes
+        ChangeMusicVolume(0);
+        ChangeSoundVolume(0);
     }
     public void PlaySound(AudioClip _sound)
     {
         soundSource.PlayOneShot(_sound);
+    }
+
+    public void ChangeSoundVolume(float _change)
+    {
+        ChangeSourceVolume(1, "soundVolume", _change, soundSource);
+    }
+    public void ChangeMusicVolume(float _change)
+    {
+        ChangeSourceVolume(0.3f, "musicVolume", _change, musicSource);
+    }
+
+    private void ChangeSourceVolume(float baseVolume, string volumeName, float change, AudioSource source)
+    {
+        //Get initial value of volume and change it
+        float currentVolume = PlayerPrefs.GetFloat(volumeName, 1);
+        currentVolume += change;
+
+        //Check if we reached the maximum or minimum value
+        if (currentVolume > 1)
+            currentVolume = 0;
+        else if (currentVolume < 0)
+            currentVolume = 1;
+
+        //Assign final value
+        float finalVolume = currentVolume * baseVolume;
+        source.volume = finalVolume;
+
+        //Save final value to player prefs
+        PlayerPrefs.SetFloat(volumeName, currentVolume);
     }
 }
